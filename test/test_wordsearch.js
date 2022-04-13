cf7e6499260512ba895bed0cfd5339476e554522
symphony <jciurc@gmail.com>
2022-04-13 16:30:08 -0600

    Completed word search function

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -18,7 +18,7 @@ describe("#wordSearch()", function() {
     ], 'FRANK')
 
     assert.isFalse(result);
-  });
+  }),
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
@@ -34,5 +34,30 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+  }),
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'R', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'A', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'K', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  }),
+  it("should return false if empty array", function() {
+    const result = wordSearch([])
+
+    assert.isFalse(result);
+  }),
+  it("should throw an error if array not provided", function() {
+
+   assert.throw(() => wordSearch(""), Error, "Not an array");
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!Array.isArray(letters)) throw TypeError("Not an array");
+  if (letters.length === 0) return false;
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = [];
+  // Transpose matrix
+  for (let row = 0; row < letters[1].length; row++) {
+    let verticalWord = "";
+    for (let column = 0; column < letters[0].length; column++) {
+      verticalWord += letters[column][row];
+    }
+    verticalJoin.push(verticalWord);
+  }
+
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
